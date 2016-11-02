5ea61de268a1ba0438dbdadaa12ddab76af93238
Jen Wenzel <s0549096@htw-berlin.de>
2016-11-02 17:16:47 +0100

    Added form validation, image upload template and split up controllers

--- app/app.js
+++ app/app.js
@@ -22,32 +22,3 @@ webShop.config(['$routeProvider', '$locationProvider', function($routeProvider, 
 // webShop.run(function(){
 //
 // });
-
-webShop.controller('InventarController', ['$scope', '$http', function($scope, $http){
-
-  $scope.removeartikel = function(artikel) {
-    var removedartikel = $scope.inventar.indexOf(artikel);
-    $scope.inventar.splice(removedartikel, 1);
-  };
-
-  $scope.addartikel = function(){
-    $scope.inventar.push({
-      name: $scope.newartikel.name,
-      kategorie: $scope.newartikel.kategorie,
-      preis: parseInt($scope.newartikel.preis),
-      available: true
-    });
-
-//reset placeholder
-    $scope.newartikel.name="";
-    $scope.newartikel.kategorie="";
-    $scope.newartikel.preis="";
-
-  };
-
-//get test data
-  $http.get('data/inventar.json').success(function(data){
-    $scope.inventar = data;
-  })
-
-}]);
