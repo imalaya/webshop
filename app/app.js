8a1747af1bed097557a30bea4286f544580f00fc
Julia Schoewe <s0543954@htw-berlin.de>
2016-11-18 13:30:07 +0100

    createInterestedPerson-Logik ausgelagert und UserController angelegt.

--- app/app.js
+++ app/app.js
@@ -46,7 +46,3 @@ webShop.config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
           controller: 'UserController'
       });
 }]);
-
-/*//Zum Testen für Julia --> wird noch ausgelagert, wenn ich weiß wohin, aber bitte noch drin lassen.
-var interestedPersonHandler = require('./core/userhandling/interestedPersonHandler');
-interestedPersonHandler.createInterestedPerson("pieps.pups@gutemine.de");*/
---
+++ app/controllers/UserController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Ju on 18.11.2016.
+ */
+//Zum Testen für Julia --> wird noch ausgelagert, wenn ich weiß wohin, aber bitte noch drin lassen.
+ var interestedPersonHandler = require('../core/userhandling/interestedPersonHandler');
+ interestedPersonHandler.createInterestedPerson("pieps.pups@gutemine.de");
