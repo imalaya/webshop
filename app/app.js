2dc8754c9b58a3818e7535cd0ffaf6663a94f8b0
Jen Wenzel <s0549096@htw-berlin.de>
2016-11-29 23:22:34 +0100

    Modals work

--- app/app.js
+++ app/app.js
@@ -44,18 +44,30 @@ webShop.config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
 
     .state('private.admin', {
         url: '/admin',
-        templateUrl:'views/home-admin.html'
+        templateUrl:'views/adminViews/home-admin.html'
       })
 
     .state('private.admin.inventory', {
         url: '/artikel-verwalten',
-        templateUrl:'views/manage-articles.html',
+        templateUrl:'views/adminViews/manage-articles.html',
         controller: 'InventoryController'
       })
 
+      .state('private.admin.inventory.add', {
+          templateUrl:'views/adminViews/new-article.html',
+          controller: 'InventoryController',
+          parent: 'private.admin.inventory'
+        })
+
+        .state('private.admin.inventory.edit', {
+            templateUrl:'views/adminViews/edit-article.html',
+            controller: 'InventoryController',
+            parent: 'private.admin.inventory'
+          })
+
       .state('private.admin.member', {
           url: '/mitarbeiter-verwalten',
-          templateUrl:'views/manage-member.html',
+          templateUrl:'views/adminViews/manage-member.html',
           controller: 'MemberController'
       });
 }]);
