903f248f197f35751d31b220edfa7420a79da755
LaurenSchulze <lauren.schulze@gmx.de>
2017-01-06 13:55:36 +0100

    backend zeugs

---
+++ public/app/controllers/CategoryCtrl.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Lauren on 05.01.17.
+ */
+webShop.controller('CategoryCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    $http.get('data/inventory.json').success(function () {
+
+    });
+
+}]);
---
+++ public/app/services/CategoryFactory.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Lauren on 05.01.17.
+ */
+webShop.factory('CategoryFactory', function ($http) {
+
+    var categories;
+
+
+});
